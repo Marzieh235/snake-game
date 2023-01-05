402f488f7103f0da258ffabfca3ffe7be9512500
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-05 03:34:39 +0330

    Create snake

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 
 
-const Gamecanvas = document.querySelector('Gamecanvas');
-const ctx = Gamecanvas.getcontext('2d');
+const Gamecanvas = document.getElementById("Gamecanvas");
+
+const ctx = Gamecanvas.getContext('2d');
 
 
-ctx.fillstyle = 'white';
-ctx.strokestyle = 'black';
+ctx.fillStyle = 'white';
+ctx.strokeStyle = 'black';
 
 
-ctx.fillrect(20, 20, Gamecanvas.with, Gamecanvas.height);
-ctx.strokeRect(0, 0, Gamecanvas.with, Gamecanvas.height)
+ctx.fillRect(0, 0, Gamecanvas.width, Gamecanvas.height);
+ctx.strokeRect(0, 0, Gamecanvas.width, Gamecanvas.height);
+
+
+
+let snake = [
+    { x: 150, y: 150 },
+    { x: 140, y: 150 },
+    { x: 130, y: 150 },
+    { x: 120, y: 150 },
+    { x: 110, y: 150 },
+]
+
+snake.forEach(snakepart => {
+
+
+    ctx.fillStyle = 'lightgreen';
+    ctx.strokeStyle = 'black';
+
+
+
+    ctx.fillRect(snakepart.x, snakepart.y, 10, 10);
+    ctx.strokeRect(snakepart.x, snakepart.y, 10, 10)
+})
