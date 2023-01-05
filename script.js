97640c1a68548a457da7fba3c8c4c6747ee01244
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-05 20:30:04 +0330

    create FOOD for snake

--- script.js
+++ script.js
@@ -33,3 +33,28 @@ snake.forEach(snakepart => {
     ctx.fillRect(snakepart.x, snakepart.y, 10, 10);
     ctx.strokeRect(snakepart.x, snakepart.y, 10, 10)
 })
+
+let foodx;
+let foody;
+let randomNumber = (max, min) => Math.round((Math.random() * (max - min) + min) / 10) * 10
+
+
+let createfood = () => {
+    foodx = randomNumber(0, Gamecanvas.width - 10);
+    foody = randomNumber(0, Gamecanvas.height - 10);
+    snake.forEach(snakepart => {
+        if (snakepart.x === foodx && snakepart.y === foody) {
+            createfood();
+        }
+    })
+}
+
+createfood();
+
+ctx.fillStyle = 'red';
+ctx.strokeStyle = 'darkred'
+
+
+
+ctx.fillRect(foodx, foody, 10, 10);
+ctx.strokeRect(foodx, foody, 10, 10)
